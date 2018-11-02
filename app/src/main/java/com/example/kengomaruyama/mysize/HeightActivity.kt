00406dc57e245ffd80b30e0c8032b0180c7a7ef9
klcatha <klcatha0518@gmail.com>
2018-11-02 16:36:28 +0900

    spinner item selected listener

--- app/src/main/java/com/example/kengomaruyama/mysize/HeightActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/mysize/HeightActivity.kt
@@ -2,11 +2,34 @@ package com.example.kengomaruyama.mysize
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_height.*
+import java.text.FieldPosition
 
 class HeightActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_height)
+
+        spinner.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener{
+                    override
+                    fun onItemSelected(parent: AdapterView<*>?,
+                                       view: View?,
+                                       position: Int,
+                                       id: Long){
+                        val spinner = parent as? Spinner
+                        val item = spinner?.selectedItem as? String
+                        item?.let {
+                            if (it.isNotEmpty()) height.text = it
+                        }
+                    }
+
+                    override
+                    fun onNothingSelected(parent: AdapterView<*>?){}
+                }
     }
 }
